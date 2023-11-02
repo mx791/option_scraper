45417a480c9d7e8904e0897f3d1538d2b184ecf4
Charles Lrz <56536939+mx791@users.noreply.github.com>
2023-11-02 16:18:18 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 python /home/ec2-user/option_scraper/scraper.py >> /home/ec2-user/option_scraper/logs.txt
 python /home/ec2-user/option_scraper/send_logs.py
-# sudo shutdown -h now
+sudo shutdown -h now

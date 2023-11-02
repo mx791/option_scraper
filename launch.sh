bda46794063c3f52270734fc75f95de8893c2552
Charles Lrz <56536939+mx791@users.noreply.github.com>
2023-11-02 16:11:12 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-python scraper.py >> logs/logs.txt
-python send_logs.py
+python /home/ec2-user/option_scraper/scraper.py >> logs/logs.txt
+python /home/ec2-user/option_scraper/send_logs.py
 # sudo shutdown -h now

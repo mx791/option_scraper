e9c0cc698234fc8ef4ac7644d8ca35927d8cded3
Charles Lrz <56536939+mx791@users.noreply.github.com>
2023-11-02 12:00:43 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/option_scraper
 git pull --force >> logs.txt
-pip install -r requirements.txt
+# pip install -r requirements.txt
 python scraper.py >> logs.txt
 python send_logs.py

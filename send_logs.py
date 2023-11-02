12c01d3b3ce2d3c1bdc952bb4d53343a971122a5
Charles Lrz <56536939+mx791@users.noreply.github.com>
2023-11-02 16:11:59 +0100

    Update send_logs.py

--- send_logs.py
+++ send_logs.py
@@ -3,4 +3,4 @@ import boto3
 s3 = boto3.client('s3')
 BUCKET_NAME = "791-options-data"
 
-s3.upload_file("logs.txt", "791-options-data", "logs.txt")
+s3.upload_file("/home/ec2-user/option_scraper/logs.txt", "791-options-data", "logs.txt")
